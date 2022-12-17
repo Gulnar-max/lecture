16fa6b60ae9e1cb864bc1d3953fa96e17b5bbeb7
Gulnar <uxukbayevagb@gmail.com>
2022-12-17 17:07:06 +0600

    add file

---
+++ lecture.py
@@ -0,0 +1,9 @@
+
+# number1 = int(input('Введите первое число '))
+# number2 = int(input('Введите второе число '))
+#
+# if number1 != number2 ** 2 and number2 != number1 ** 2:
+#     print('no')
+# else:
+#     print('yes')
+
